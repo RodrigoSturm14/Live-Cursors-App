dbdd32195e114859c7f3ac3379ba5c0c6ddd1820
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-20 19:22:35 -0300

    Created broadcast - each user will recieve the updated position of each user's mouse

--- server/index.js
+++ server/index.js
@@ -8,15 +8,31 @@ const uuidv4 = require("uuid").v4
 const server = http.createServer()
 const wsServer = new WebSocketServer({ server })
 const port = 8000
-// diccionario para guardar los usuarios conectados y hacer un 
-// bradcast; loop q itera cada uuid con connection.send
+// diccionario q guarda las uuid; las uuid guardan las "connection" de cada usuario conectado
+// sirve para hacer un brodcast; loop q itera cada uuid y manda la posicion del mouse con "connection.send"
 const connections = { }
 // diccionario para los usuarios conectados y la posicion del mouse
-const user = { }
+const users = { }
+
+const brodcast = () =>{
+  // convirtiendo connections en un array e itereandolo
+  Object.keys(connections).forEach(uuid => {
+    // guardando cada "connection" de los uuid en "connection"
+    const connection = connections[uuid]
+    console.log(connection)
+    // mandar el mensaje actualizado de la posicion del mouse a cada cliente
+    const message = JSON.stringify(users)
+    connection.send(message)
+  })
+}
 
 const handleMessage = (bytes, uuid) => { // los mensajes llegan como bytes y hay q pasarlos a formato json
   const message = JSON.parse(bytes.toString())
+  // actualizar posicion del mouse del usuario que mando la posicion del mouse
+  const user = users[uuid]
   user.state = message
+
+  brodcast()
   console.log(message)
   // { "x":150, "y":130 }
 }
@@ -32,11 +48,12 @@ wsServer.on("connection", (connection, request) =>{
   console.log(username)
   console.log(uuid)
 
-  connection[uuid] = connection
+  connections[uuid] = connection
+  console.log(connections)
 
-  user[uuid] = {
-    "username": username,
-    "state": { }
+  users[uuid] = {
+    username: username,
+    state: { }
   }
 
   connection.on("message", message => handleMessage(message, uuid))
