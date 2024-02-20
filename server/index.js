358226e846a82597d8731d4588ea5fd8e500b734
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-20 15:08:20 -0300

    Added WS event connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const http = require('http')
 const { WebSocketServer } = require('ws')
+const url = require('url')
 
 const server = http.createServer()
 const wsServer = new WebSocketServer({ server })
 const port = 8000
+
+wsServer.on("connection", (connection, request) =>{
+  // ws://localhost:8000?username=rodri --> el ? es una query
+  const { username } = url.parse(request.url, true).query
+  console.log(username)
+})
 
 server.listen(port, () => {
   console.log(`WebSocket server running on port ${port}`)
