335ea57e437b13d29d7359e1df5746658366fb7b
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-23 12:51:49 -0300

    Update: Home component is able to render user list and users's position with renderUserList

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -8,7 +8,7 @@ export function Home({ username }) {
   const WS_URL = 'ws://127.0.0.1:8000'
   // useWebSocket se usa para inicializar la conexion ws con el servidor; necesita url, q se le puede
   // agregar una query --> el hook devuelve funcion sendJSONmessage para mandar mensajes al servidor
-  const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
+  const { sendJsonMessage, lastJsonMessage, sendMessage } = useWebSocket(WS_URL, {
     queryParams: { username }
   })
 
@@ -24,10 +24,30 @@ export function Home({ username }) {
       )
     })
   }
+  // renderizar lista de usuarios y posiciones
+  const renderUserList = ( users ) => {
+    return(
+      <ul>
+        {
+          Object.keys(users).map((uuid) => {
+            return(
+              <li key={uuid}>
+                {
+                  JSON.stringify(users[uuid])
+                }
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
 
   useEffect(() =>{
 
     console.log('hola render')
+    // mandar mensaje al server ni bien el client se conecta al websocket para hacerle saber al otro user
+    // q otro user esta conectado
     sendJsonMessage({
       x: 0,
       y: 0
@@ -36,6 +56,7 @@ export function Home({ username }) {
     // ocurra el eventos, se envia mensaje json 
     // la subcripcion al evento se ejecuta una sola vez, por q esta en el useEffect
     window.addEventListener("mousemove", (e) =>{
+      // .current por q 'sendJsonMessageThrottled' pasa a ser objeto de useRef
       sendJsonMessageThrottled.current({ 
         x: e.clientX,
         y: e.clientY
@@ -48,6 +69,7 @@ export function Home({ username }) {
     return(
       <>
         {renderCursors(lastJsonMessage)}
+        {renderUserList(lastJsonMessage)}
       </>
     )
   }
